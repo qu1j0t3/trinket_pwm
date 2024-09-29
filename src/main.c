c7a08a2d23a6fbe90797c0fec8075130aaa5a6b9
Toby Thain <toby@telegraphics.net>
2024-09-29 11:05:13 -0400

    Initial commit.

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/* I/O map:
+    output channel
+
+    For our PIR's
+    * red cable is + voltage power,
+    * black cable is - ground power and
+    * yellow is the signal out
+ */
+
+int main() {
+    return EXIT_SUCCESS;
+}
